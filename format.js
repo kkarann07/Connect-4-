07bf4c758c09c0754e7863b69d971a0c9d248aeb
Karan Raj <33369747+kkarann07@users.noreply.github.com>
2019-01-27 19:22:04 +0530

    Update format.js

--- format.js
+++ format.js
@@ -1,4 +1,3 @@
-// We need to use jQuery for the following:
 
 var player1 = prompt("Player One: Enter Your Name , you will be Blue");
 var player1Color = 'rgb(86, 151, 255)';
@@ -9,18 +8,17 @@ var player2Color = 'rgb(237, 45, 73)';
 var game_on = true;
 var table = $('table tr');
 
-// http://stackoverflow.com/questions/6139407/getting-td-by-index-with-jquery
 function reportWin(rowNum,colNum) {
   console.log("You won starting at this row,col");
   console.log(rowNum);
   console.log(colNum);
 }
-// Change the color of a button
+
 function changeColor(rowIndex,colIndex,color) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color);
 }
 
-// Report Back to current color of a button
+
 function returnColor(rowIndex,colIndex) {
   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
 }
@@ -141,30 +139,3 @@ $('.board button').on('click',function() {
 
 })
 
-
-
-// Helper function to help you understand Rows and Columns From A Table
-// http://stackoverflow.com/questions/788225/table-row-and-column-number-in-jquery
-//
-// $('.board button').on('click',function(){
-//   // This is the Column Number (starts at zero):
-//   console.log('This is the Column:');
-//   console.log($(this).closest("td").index());
-//   // This is the Row Number:
-//   console.log("This is the Row:");
-//   console.log($(this).closest("tr").index());
-//   console.log('\n');
-//   // This is a way to grab a particular cell (replace):
-//   // $('table').eq(rowIndex).find('td').eq(colIndex)
-// });
-
-// // Change color on click
-// $('.board button').on('click',function() {
-//   if($(this).css('background-color') === 'rgb(51, 51, 51)'){
-//     $(this).css('background-color','rgb(86, 151, 255)');
-//   }else if ($(this).css('background-color') === 'rgb(86, 151, 255)'){
-//     $(this).css('background-color','rgb(237, 45, 73)');
-//   }else{
-//     $(this).css('background-color','rgb(51, 51, 51)');
-//   }
-// });
